20acd21e589d85265086b9e35c4cdb21efc7e244
w-haibara <hwhaibarawataru@gmail.com>
2020-02-21 20:18:08 +0900

    booeanを渡すのに:を付け忘れてた

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <v-content>
 
       <v-row  id="home">
-        <v-carousel cycle height="200" interval="3000" hide-delimiter-background hide-delimiters show-arrows="false" show-arrows-on-hover="false">
+        <v-carousel cycle height="200" interval="3000" hide-delimiter-background hide-delimiters :show-arrows='false' :show-arrows-on-hover='false'>
           <v-carousel-item v-for="(item,i) in items" :key="i" :src='item'>
           </v-carousel-item>
         </v-carousel>
