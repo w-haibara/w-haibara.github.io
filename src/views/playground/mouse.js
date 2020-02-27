f1c86ded3f2466ef0e7550e5929abfdb17a06cf4
w-haibara <hwhaibarawataru@gmail.com>
2020-02-27 18:46:31 +0900

    インデント等を修正

--- src/views/playground/mouse.js
+++ src/views/playground/mouse.js
@@ -2,14 +2,14 @@ let x = 0
 let y = 0
 
 function showMouseVals(id) {
-    var el = document.getElementById(id);
-    el.onmousemove = function(event) {
-        event = event || window.event
-        //console.log(event.clientX + ", " + event.clientY)
-        x = event.offsetX
-        y = event.offsetY
-    }
-    return {x, y}
+  var el = document.getElementById(id);
+  el.onmousemove = function (event) {
+    event = event || window.event
+    //console.log(event.clientX + ", " + event.clientY)
+    x = event.offsetX
+    y = event.offsetY
+  }
+  return { x, y }
 }
 
 export { showMouseVals }
