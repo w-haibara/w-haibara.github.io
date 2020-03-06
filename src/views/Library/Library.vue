1ee28d15624a04bad6c06109f4d0640bbedf7206
w-haibara <hwhaibarawataru@gmail.com>
2020-03-06 19:30:34 +0900

    LibraryのUIを改善

--- src/views/Library/Library.vue
+++ src/views/Library/Library.vue
@@ -9,14 +9,26 @@
         </v-img>
       </v-col>
 
-      <v-container>
-        <v-container>
-          <v-row>
-            <v-carousel height="360">
-              <v-carousel-item v-for="(item,i) in items" :key="i">
-                <v-card class="mx-auto" max-width="480" height="299">
+      <ul class="horizontal-list">
+        <li>
+          <h1>Slides</h1>
+        </li>
+        <li v-for="(slide,i) in slides" :key="i">
+          <div>
+            <v-container>
+              <v-card
+                width="400"
+                min-height="200"
+                :loading="slide.loading"
+                v-on:click="slide.dialog=true"
+              >
+                <v-card-title>{{ slide.title }}</v-card-title>
+                <v-card-text>{{ slide.text }}</v-card-text>
+              </v-card>
+              <v-dialog v-model="slide.dialog" width="480" height="299">
+                <v-card width="480" height="299">
                   <iframe
-                    :src="item.src"
+                    :src="slide.src"
                     frameborder="0"
                     width="480"
                     height="299"
@@ -25,34 +37,22 @@
                     webkitallowfullscreen="true"
                   ></iframe>
                 </v-card>
-              </v-carousel-item>
-            </v-carousel>
-          </v-row>
-        </v-container>
-      </v-container>
+              </v-dialog>
+            </v-container>
+          </div>
+        </li>
+      </ul>
 
       <ul class="horizontal-list">
+        <li>
+          <h1>Brog</h1>
+        </li>
         <li v-for="(article,i) in articles.names" :key="i">
           <div>
             <v-container>
-              <v-hover v-slot:default="{ hover }">
-                <v-card
-                  class="mx-auto"
-                  color="cyan lighten-4"
-                  width="400"
-                  min-height="200"
-                  v-on:click="$router.push('article/'+article)"
-                >
-                  <v-expand-transition>
-                    <div
-                      v-if="hover"
-                      class="d-flex transition-fast-in-fast-out black darken-2 v-card--reveal display-3 white--text"
-                      style="height: 100%;"
-                    ></div>
-                  </v-expand-transition>
-                  <v-card-title>{{ articles.titles[i] }}</v-card-title>
-                </v-card>
-              </v-hover>
+              <v-card width="400" min-height="150" v-on:click="$router.push('article/'+article)">
+                <v-card-title>{{ articles.titles[i] }}</v-card-title>
+              </v-card>
             </v-container>
           </div>
         </li>
@@ -62,30 +62,43 @@
 </template>
 
 <script>
+import axios from "axios";
 import Posts from "./posts/posts.json";
 
 export default {
   data: () => ({
-    items: [
+    slides: [
       {
         src:
           "https://docs.google.com/presentation/d/e/2PACX-1vRPPGZz4zBfKcz1dMu4pnATGQh2ZYXRUJIL7QKuvgxygGB0ZJIBMSJ5elCOya-tasHictLT7brYrDk4/embed?start=false&loop=false&delayms=3000",
-        loaded: false
+        loading: true,
+        title: "",
+        text: "",
+        dialog: false
       },
       {
         src:
           "https://docs.google.com/presentation/d/e/2PACX-1vQAO0BBiAk8wFS1kSwg2zXbc0G_9E0QNgpdgBOSQmMXhVwBtUwt8jVxitXEl6RvXxEts-T-Bg_vb-wA/embed?start=false&loop=false&delayms=3000",
-        loaded: false
+        loading: true,
+        title: "",
+        text: "",
+        dialog: false
       },
       {
         src:
           "https://docs.google.com/presentation/d/e/2PACX-1vQRCrDQD_Vy8-V63iL7owU4YayRBzZvz472usc47SFnbLYaWvxVR6n2Hea3SJ7MrAoCm1lGjUIsPSIc/embed?start=false&loop=false&delayms=3000",
-        loaded: false
+        loading: true,
+        title: "",
+        text: "",
+        dialog: false
       },
       {
         src:
           "https://docs.google.com/presentation/d/e/2PACX-1vQjR0Gb67JgXhzVgnXlpiiH2oF4T0s1mvr658_2gX2wyV4VI6cDVBKhzOH6GH6DNR91OPUNtRSScOTq/embed?start=false&loop=false&delayms=3000",
-        loaded: false
+        loading: true,
+        title: "",
+        text: "",
+        dialog: false
       }
     ]
   }),
@@ -99,16 +112,36 @@ export default {
         }
         return { names: Posts.posts, titles: titles };
       }
-    },
-    getTitle: {
-      get() {
-        let mdName = 1;
-        const source = require("./posts/" + mdName + ".md");
-        return source.slice(2, source.indexOf("\n", 0));
+    }
+  },
+  methods: {
+    setSlideData: function() {
+      for (let i in this.slides) {
+        axios
+          .get(this.slides[i].src)
+          .then(response => {
+            this.slides[i].title = response.data.match(
+              /<meta property="og:title" content="(.*?)"*?>/
+            )[1];
+
+            this.slides[i].text = response.data.match(
+              /<meta property="og:description" content="(.*?)"*?>/
+            )[1];
+
+            this.slides[i].loading = false;
+          })
+          .catch(err => {
+            console.log("axios_err:", err);
+
+            //this.slides[i].loading = false;
+          });
       }
     }
   },
-  method: {}
+  created() {
+    this.setSlideData();
+  },
+  mounted() {}
 };
 </script>
 
