d8cf37771d900187a5dec4b61fc3b278e08439c2
w-haibara <hwhaibarawataru@gmail.com>
2020-03-06 21:17:34 +0900

    setPosts.shをルート直下に移動

--- setPosts.sh
+++ setPosts.sh
@@ -1,3 +1,4 @@
+cd ./src/views/Library/posts/
 echo "{ \"posts\":[" > posts.json
 ls | grep .md | sed -e 's/.md//' | while read line; do echo \"${line}\",; done | sed '$s/.$//' >> posts.json
 echo ]} >> posts.json
