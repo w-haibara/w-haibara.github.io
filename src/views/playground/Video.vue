2f360d1110edecca840ce68c699fd9c7ed9ca66f
w-haibara <hwhaibarawataru@gmail.com>
2020-05-01 00:28:56 +0900

    Update Video.vue

--- src/views/playground/Video.vue
+++ src/views/playground/Video.vue
@@ -1,8 +1,12 @@
 <template>
   <v-app id>
     <v-content>
-      <v-switch v-model="mode.gray" label="gray"></v-switch>
-
+      <v-row>
+        <v-switch v-model="mode.gray" flat label="グレースケール"></v-switch>
+        <v-switch v-model="mode.nega" flat label="ネガポジ反転"></v-switch>
+        <v-switch v-model="mode.enhanceFil" flat label="Sobel 水平フィルタ"></v-switch>
+        <v-switch v-model="mode.laplaceFil" flat label="ラプラシアンフィルタ"></v-switch>
+      </v-row>
       <canvas id="canvas"></canvas>
     </v-content>
   </v-app>
@@ -15,96 +19,110 @@ export default {
     mode: {
       gray: false,
       nega: false,
-      enhanceFil: false
+      enhanceFil: false,
+      laplaceFil: false
     },
-    canvas: null,
-    ctx: null,
-    offscreen: null,
-    offscreenCtx: null,
-    video: null,
-    stream: null
+    width: 600,
+    height: 600,
+    video: {},
+    canvas: {},
+    captures: []
   }),
   methods: {
-    convolution: function(data, i, a1, a2, a3, a4, a5, a6, a7, a8, a9) {
-      for (let color = 0; color < 3; color++) {
-        data[i + color] +=
-          i - 4 - this.offscreen.width * 4 + color < 0 ||
-          i + 4 + this.offscreen.width * 4 + color > data.length
-            ? data[i + color]
-            : data[i - 4 - this.offscreen.width * 4 + color] * a1 +
-              data[i - this.offscreen.width * 4 + color] * a2 +
-              data[i + 4 - this.offscreen.width * 4 + color] * a3 +
-              data[i - 4 + color] * a4 +
-              data[i + color] * a5 +
-              data[i + 4 + color] * a6 +
-              data[i - 4 + this.offscreen.width * 4 + color] * a7 +
-              data[i + this.offscreen.width * 4 + color] * a8 +
-              data[i + 4 + this.offscreen.width * 4 + color] * a9;
-      }
-    },
-    enhanceFil: function(data) {
-      for (let i = 0; i < data.length; i += 4) {
-        this.convolution(data, i, -1, -2, -1, 0, 0, 0, 1, 2, 1);
-      }
-    },
-    nega: function(data) {
-      for (let i = 0; i < data.length; i += 4) {
-        data[i] = 255 - data[i];
-        data[i + 1] = 255 - data[i + 1];
-        data[i + 2] = 255 - data[i + 2];
-      }
-    },
-    gray: function(data) {
-      for (let i = 0; i < data.length; i += 4) {
-        const color = (data[i] + data[i + 1] + data[i + 2]) / 3;
-        data[i] = data[i + 1] = data[i + 2] = color;
-      }
-    },
-    filter: function(data) {
-      if (this.mode.gray) {
-        this.gray(data);
-      }
-      if (this.mode.nega) {
-        this.nega(data);
-      }
-      if (this.mode.enhanceFil) {
-        this.enhanceFil(data);
-      }
-    },
-    tick: function() {
-      this.offscreenCtx.drawImage(this.video, 0, 0);
-      const imageData = this.offscreenCtx.getImageData(
-        0,
-        0,
-        this.offscreen.width,
-        this.offscreen.height
-      );
-      this.filter(imageData.data);
-      this.offscreenCtx.putImageData(imageData, 0, 0);
-      this.ctx.drawImage(this.offscreen, 0, 0);
-      window.requestAnimationFrame(this.tick);
-    },
-    getData: function() {
-      this.video.play();
-      this.canvas.width = this.offscreen.width = this.video.videoWidth;
-      this.canvas.height = this.offscreen.height = this.video.videoHeight;
-      this.tick();
-    },
-    async main() {
-      this.canvas = document.getElementById("canvas");
-      this.ctx = this.canvas.getContext("2d");
-      this.offscreen = document.createElement("canvas");
-      this.offscreenCtx = this.offscreen.getContext("2d");
-      this.video = document.createElement("video");
-      this.stream = await navigator.mediaDevices.getUserMedia({
+    async main(mode) {
+      const canvas = document.getElementById("canvas");
+      const ctx = canvas.getContext("2d");
+      const offscreen = document.createElement("canvas");
+      const offscreenCtx = offscreen.getContext("2d");
+      const video = document.createElement("video");
+      const stream = await navigator.mediaDevices.getUserMedia({
         video: true
       });
-      this.video.srcObject = this.stream;
-      this.video.onloadedmetadata = this.getData();
+      video.srcObject = stream;
+      video.onloadedmetadata = () => {
+        video.play();
+        canvas.width = offscreen.width = video.videoWidth;
+        canvas.height = offscreen.height = video.videoHeight;
+        tick();
+      };
+
+      function tick() {
+        offscreenCtx.drawImage(video, 0, 0);
+        const imageData = offscreenCtx.getImageData(
+          0,
+          0,
+          offscreen.width,
+          offscreen.height
+        );
+        filter(imageData.data);
+        offscreenCtx.putImageData(imageData, 0, 0);
+        ctx.drawImage(offscreen, 0, 0);
+        window.requestAnimationFrame(tick);
+      }
+
+      function filter(data) {
+        if (mode.gray) {
+          gray(data);
+        }
+        if (mode.nega) {
+          nega(data);
+        }
+        if (mode.enhanceFil) {
+          enhanceFil(data);
+        }
+        if (mode.laplaceFil) {
+          laplaceFil(data);
+        }
+      }
+
+      function gray(data) {
+        for (let i = 0; i < data.length; i += 4) {
+          const color = (data[i] + data[i + 1] + data[i + 2]) / 3;
+          data[i] = data[i + 1] = data[i + 2] = color;
+        }
+      }
+
+      function nega(data) {
+        for (let i = 0; i < data.length; i += 4) {
+          data[i] = 255 - data[i];
+          data[i + 1] = 255 - data[i + 1];
+          data[i + 2] = 255 - data[i + 2];
+        }
+      }
+
+      function enhanceFil(data) {
+        for (let i = 0; i < data.length; i += 4) {
+          convolution(data, i, -1, -2, -1, 0, 0, 0, 1, 2, 1);
+        }
+      }
+
+      function laplaceFil(data) {
+        for (let i = 0; i < data.length; i += 4) {
+          convolution(data, i, 0, 1, 0, 1, -4, 1, 0, 1, 0);
+        }
+      }
+
+      function convolution(data, i, a1, a2, a3, a4, a5, a6, a7, a8, a9) {
+        for (let color = 0; color < 3; color++) {
+          data[i + color] +=
+            i - 4 - offscreen.width * 4 + color < 0 ||
+            i + 4 + offscreen.width * 4 + color > data.length
+              ? data[i + color]
+              : data[i - 4 - offscreen.width * 4 + color] * a1 +
+                data[i - offscreen.width * 4 + color] * a2 +
+                data[i + 4 - offscreen.width * 4 + color] * a3 +
+                data[i - 4 + color] * a4 +
+                data[i + color] * a5 +
+                data[i + 4 + color] * a6 +
+                data[i - 4 + offscreen.width * 4 + color] * a7 +
+                data[i + offscreen.width * 4 + color] * a8 +
+                data[i + 4 + offscreen.width * 4 + color] * a9;
+        }
+      }
     }
   },
   mounted() {
-    this.main();
+    this.main(this.mode);
   }
 };
 </script>
