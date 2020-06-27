100973c58edce06313a724a639a3d983e6f73d5b
w-haibara <39176215+w-haibara@users.noreply.github.com>
2020-06-27 15:37:15 +0900

    Update Slides.vue

--- src/views/Slides.vue
+++ src/views/Slides.vue
@@ -43,6 +43,7 @@ class slide {
 export default {
   data: () => ({
     ids: [
+      "2PACX-1vT6wJrKx6gGsV_tLoeIbnyNPRD9f4Yu1l2HlVj8QzFNz_CsoLYkVL8DeYkeSk2XwnVsp-DxtY-Rx29S",
       "2PACX-1vQDkWnRyodeo-X_4EOuiWTyOlRN8e33hAz8SWERK4n9GMTVUG53-p0gJHVzWNtbqHUE33QvSjw9KjiB",
       "2PACX-1vSkb3gsaoyCpDjz-D_O3tluDR-8HJuO80nzwe0GX4KPLEacfKVaEZ3Cq2-brWmS16Bn6_JbzbP9hNM6",
       "2PACX-1vT1XIpafcbjijZVInZA6T3LF9BmMA-pM8ijH1Uiy6WRLMmyC2XUyhB_vqdpROYv3TKdhHo8haK_FtEX",
